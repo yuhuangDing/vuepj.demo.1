1ebbb5006b2dafc4fe1a884a4609e20a63455872
cz-dyh <164848632@qq.com>
2020-02-22 23:11:15 +0800

    完成首页设计， 已知bug：首页图片展示存在问题 解决已知bug： webpack.config.js文件中的url-loader加入esmodule选项启用CommonJS模块语法 {test: /\.(jpg|png|jpeg|bmp)$/, use:[ {loader: 'url-loader',options: {esModule:false}}] },//处理图片url,
    Signed-off-by: cz-dyh <164848632@qq.com>

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports={
         rules: [
             {test:/\.vue$/,use:'vue-loader'},//处理vue文件
             {test:/\.css$/,use:['style-loader' ,'css-loader']},//处理css文件的loader
-            {test: /\.(jpg|png|jpeg|bmp)$/, use: 'url-loader'},//处理图片url,
+            {test: /\.(jpg|png|jpeg|bmp)$/, use:[ {loader: 'url-loader',options: {esModule:false}}] },//处理图片url,
             {test:/\.(ttf|eot|svg|woff|woff2)$/,use:'url-loader'},//处理字体的loader
             {test:/.\.js$/,use:'babel-loader',exclude:/node_modules/},//配置babel的规则
 
